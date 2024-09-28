e8de533b64bb7f095336a34734c8d7de105c8f83
Dmitrii <128043391+dkalenov@users.noreply.github.com>
2024-09-28 09:59:34 +0700

    main.py

--- spoting_bot_binance/main.py
+++ spoting_bot_binance/main.py
@@ -1,1 +1,9 @@
-
+import sloping
+import io
+from datetime import datetime, timezone
+from dateutil.relativedelta import relativedelta
+import os
+import requests
+import zipfile
+import csv
+import plotly.graph_objects as go
