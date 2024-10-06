0b02d6f443b40f605d280762792c4710fe62d114
Dmitrii <128043391+dkalenov@users.noreply.github.com>
2024-10-06 23:25:41 +0700

    Update main.py

--- sloping_bot_binance/main.py
+++ sloping_bot_binance/main.py
@@ -1,37 +1,42 @@
+import asyncio
+import configparser
+import traceback
 import sloping
-import io
-from datetime import datetime, timezone
-from dateutil.relativedelta import relativedelta
-import os
-import requests
-import zipfile
-import csv
-import plotly.graph_objects as go
+import db
 
 
+# импортируем конфиг
+config = configparser.ConfigParser()
+config.read('config.ini')
+# конфиг файл
+conf: db.ConfigInfo
 
-symbol = 'BTCUSDT'
-interval = '1h'
-start_date = '2024-04'
-end_date = '2024-08'
-lenght = 50
 
-def generate_months(start_date, end_date):
-    # get start and end dates
-    start_date = datetime.strptime(start_date, '%Y-%m')
-    end_date = datetime.strptime(end_date, '%Y-%m')
-    months = []
-    # loop through months
-    while start_date <= end_date:
-        months.append(start_date.strftime('%Y-%m'))
-        # add month
-        start_date = start_date + relativedelta(months=1)
-    return months
+session: db.async_sessionmaker
 
-months = generate_months(start_date, end_date)
 
-# create folders if they don't exist
-if not os.path.exists('klines'):
-    os.mkdir('klines')
-if not os.path.exists('plots'):
-    os.mkdir('plots')
+async def main():
+    global session
+    global conf
+    global client
+    # подключаемся к базе данных
+    session = await db.connect(config['DB']['host'], int(config['DB']['port']), config['DB']['user'],
+                         config['DB']['password'], config['DB']['db'])
+    # загружаем конфиг
+    conf = await db.load_config()
+    # создаем клиента для Binance
+    client = binance.Futures(conf.api_key, conf.api_secret, asynced=True,
+                             testnet=config.getboolean('BOT', 'testnet'))
+    # загружаем все торговые пары
+    asyncio.create_task(load_symbols())
+    # подключаемся к вебсокетам
+    asyncio.create_task(connect_ws())
+    # запускаем бота телеграм
+    await tg.run(session, client, connect_ws, disconnect_ws, subscribe_ws, unsubscribe_ws, remove_indicator)
+
+
+if __name__ == '__main__':
+    # загружаем конфиг
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    asyncio.run(main())
