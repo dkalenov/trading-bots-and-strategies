321ba6906b6d53551d8d04202ea04fd17bff85b7
Dmitrii <128043391+dkalenov@users.noreply.github.com>
2024-10-06 23:26:56 +0700

    sloping.py

---
+++ sloping_bot_binance/sloping.py
@@ -0,0 +1,10 @@
+# класс для поиска наклонок
+class Sloping:
+    length: int
+    ts: deque
+    open: deque
+    high: deque
+    low: deque
+    close: deque
+    body_up: deque
+    body_down: deque
