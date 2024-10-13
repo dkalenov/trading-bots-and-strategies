7e18c38c81a9950b30baff850c8b7ce5eeebb14c
Dmitrii <128043391+dkalenov@users.noreply.github.com>
2024-10-14 00:06:25 +0700

    db.py

--- density_bot_binance/db.py
+++ density_bot_binance/db.py
@@ -1,1 +1,8 @@
+from sqlalchemy import Column, Integer, BigInteger, String, Float, Boolean, select, delete, update
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
+
+# создаем базовый класс для работы с БД
+Base = declarative_base()
+Session: async_sessionmaker
