5e3ee01aa0897dca50815013be38cd188541bc9a
Dmitrii <128043391+dkalenov@users.noreply.github.com>
2024-10-13 23:59:27 +0700

    main.py

--- density_bot_binance/main.py
+++ density_bot_binance/main.py
@@ -1,1 +1,18 @@
+import configparser
+import binance
+import db
+import asyncio
 
+
+# функция запуска бота
+async def main():
+    # глобальные переменные
+    global client
+    global all_symbols
+    global conf
+    global session
+    # подключаемся к базе данных
+    session = await db.connect(config['DB']['host'], int(config['DB']['port']), config['DB']['user'],
+                               config['DB']['password'], config['DB']['db'])
+    # загружаем конфиг
+    conf = await db.load_config()
