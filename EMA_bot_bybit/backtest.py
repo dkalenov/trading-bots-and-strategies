2dd5481e7228c3da54a7e6c36197e97a3cc87c79
Dmitrii <128043391+dkalenov@users.noreply.github.com>
2024-10-20 23:00:16 +0700

    backtest.py

--- EMA_bot_bybit/backtest.py
+++ EMA_bot_bybit/backtest.py
@@ -30,3 +30,40 @@ def generate_months(start_date, end_date):
     return months
 
 months = generate_months(start_date, end_date)
+
+
+# создаем папки если их нет
+if not os.path.exists('klines'):
+    os.mkdir('klines')
+
+klines = {
+    'Date': [],
+    'Open': [],
+    'High': [],
+    'Low': [],
+    'Close': [],
+    'Volume': []
+}
+
+for month in months:
+    filename = f"{symbol}-{interval}-{month}.zip"
+    # если нет klines, то скачиваем
+    if not os.path.exists(f"klines/{filename}"):
+        url = f"https://data.binance.vision/data/futures/um/monthly/klines/{symbol}/{interval}/{filename}"
+        r = requests.get(url, allow_redirects=True)
+        open(f"klines/{filename}", 'wb').write(r.content)
+    # открываем zip
+    with zipfile.ZipFile(f"klines/{filename}", 'r') as zip_file:
+        # читаем csv внутри zip
+        with zip_file.open(f"{symbol}-{interval}-{month}.csv", 'r') as csv_file:
+            csv_reader = csv.reader(io.TextIOWrapper(csv_file, 'utf-8'))
+            for row in csv_reader:
+                # если строка содержит число (отбрасываем первую строку)
+                if row[0].isdigit():
+                    # заполняем списки с данными свечи
+                    klines['Date'].append(datetime.fromtimestamp(int(row[0]) / 1000, tz=timezone.utc))
+                    klines['Open'].append(float(row[1]))
+                    klines['High'].append(float(row[2]))
+                    klines['Low'].append(float(row[3]))
+                    klines['Close'].append(float(row[4]))
+                    klines['Volume'].append(float(row[5]))
