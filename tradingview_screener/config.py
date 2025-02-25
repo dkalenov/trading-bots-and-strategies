b56a728155ca605b74f9fb38f8110334a6becd35
Dmitrii <128043391+dkalenov@users.noreply.github.com>
2025-02-25 23:23:14 +0700

    second commit

--- tradingview_screener/config.py
+++ tradingview_screener/config.py
@@ -1,2 +1,10 @@
 TELEGRAM_TOKEN = "YOUR_TELEGRAM_TOKEN"
 TELEGRAM_CHANNEL = "YOUR_TELEGRAM_CHANNEL"
+
+ DB_CONFIG = {
+    "dbname": "your_db",
+    "user": "your_username",
+    "password": "your_password",
+    "host": "localhost",
+    "port": "5432",
+}
